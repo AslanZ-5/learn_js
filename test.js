027c273f294e29ad384129a3b91b3842a58d83b2
мансур зурабов <asl.zurabov@gmail.com>
2022-09-08 11:11:05 +0300

    map and filter

--- test.js
+++ test.js
@@ -147,35 +147,26 @@
 // let dic = { 2:1,d:34}
 // console.log(2 in dic)
 
-function score( dice ) {
-    points = 0
-    dic = {}
-    for (let i in dice){
-      if (dice[i] in dic){
-        dic[dice[i]] += 1
-      }else{
-        dic[dice[i]] = 1
-  }
-    }
-    console.log(dic)
-    if ('6' in dic && dic['6'] == 3){
-      points += 600
-    }
-    if ('4' in dic && dic['4'] == 3){
-      points += 400
-    }
-    if ('3' in dic && dic['3'] == 3){
-      points += 300
-    }
-    if ('2' in dic && dic['2'] == 3){
-      points += 200
-    }
-    if ('1' in dic){
-      points += (Math.floor(dic['1']/ 3) * 1000) + ((dic[1] % 3) * 100)
-    }
-    if ('5' in dic){
-      points += (Math.floor(dic['5']/ 3) * 1000) + ((dic[5] % 3) * 50)
-    }
-    return points
-  }
-  console.log(score([2, 4, 4, 5, 4]))
+// new looping way
+
+const a = ['banana', 'cherry', 'apple', 'watermelon']
+
+for ( fruit of a){
+  console.log(fruit)
+}
+
+// map function
+
+let ab = [1,2,3,4,5,6,7]
+
+console.log(ab.map(fn => fn ** 2))
+
+
+const actors = [
+  {name:'John weik',netWorth:122222},
+  {name:'leonardo',netWorth:1022},
+  {name:'antonio banderos',netWorth:100},
+  {name:'bread bit',netWorth:199999}
+]
+
+console.log(actors.filter(ntW => ntW.netWorth > 1000))
