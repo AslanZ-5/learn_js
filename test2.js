f40d470f2b733f3864b9f31014e02e6da3a662bc
мансур зурабов <asl.zurabov@gmail.com>
2022-09-08 14:02:42 +0300

    DOM onclick function

--- test2.js
+++ test2.js
@@ -1,8 +1,16 @@
-console.log(document.getElementById('title'))
-console.log(document.getElementById('title').innerText)
-document.getElementById('title').innerText = 'How are you ?'
-const title = document.getElementById('title')
-let mes = 'the new message'
-title.innerHTML = `<p> ${mes} </p>`
-title.style.color = 'red'
-title.style.background = 'gray'
+// console.log(document.getElementById('title'))
+// console.log(document.getElementById('title').innerText)
+// document.getElementById('title').innerText = 'How are you ?'
+// const title = document.getElementById('title')
+// let mes = 'the new message'
+// title.innerHTML = `<p> ${mes} </p>`
+// title.style.color = 'red'
+// title.style.background = 'gray'
+
+
+let rd = document.getElementById('red')
+let yel = document.getElementById('yellow')
+let grn = document.getElementById('green')
+rd.onclick = () => console.log('you clicked on red')
+yel.onclick = () => console.log('you clicked on yellow')
+grn.onclick = () => console.log('you clicked on green')
