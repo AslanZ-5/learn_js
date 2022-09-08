5a00a3bc1be15d3b09d5f9d0a9c53791b6c8edf7
мансур зурабов <asl.zurabov@gmail.com>
2022-09-08 14:20:42 +0300

    forEach function

--- test2.js
+++ test2.js
@@ -8,9 +8,16 @@
 // title.style.background = 'gray'
 
 
-let rd = document.getElementById('red')
-let yel = document.getElementById('yellow')
-let grn = document.getElementById('green')
-rd.onclick = () => console.log('you clicked on red')
-yel.onclick = () => console.log('you clicked on yellow')
-grn.onclick = () => console.log('you clicked on green')
+// let rd = document.getElementById('red')
+// let yel = document.getElementById('yellow')
+// let grn = document.getElementById('green')
+// rd.onclick = () => console.log('you clicked on red')
+// yel.onclick = () => console.log('you clicked on yellow')
+// grn.onclick = () => console.log('you clicked on green')
+
+const squares = document.querySelectorAll('.colorsquare')
+console.log(squares[0].value)
+
+squares.forEach(square => {
+    square.onclick = () => console.log(square.value)
+})
