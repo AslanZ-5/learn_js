a2aea92253b56cadc2986a557cf1c40a8298267c
мансур зурабов <asl.zurabov@gmail.com>
2022-09-10 13:55:09 +0300

    js async

--- promises/script.js
+++ promises/script.js
@@ -8,8 +8,17 @@ const promise1 = new Promise((resolve,reject) => {
     }, 1000)
 } )
 
-console.log(
-    promise1
-    .then( success => console.log({success}))
-    .catch(error => console.log({error}))
-)
+// console.log(
+//     promise1
+//     .then( success => console.log({success}))
+//     .catch(error => console.log({error}))
+// )
+
+const getDog = async () => {
+    const url = 'https://akabab.github.io/superhero-api/api/id/2.json'
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+}
+
+getDog()
