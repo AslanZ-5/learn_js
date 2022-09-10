fdfb09e8ca430d6e78bd2b2948986076e1549912
мансур зурабов <asl.zurabov@gmail.com>
2022-09-10 11:34:34 +0300

    show skills

--- supeApi/script.js
+++ supeApi/script.js
@@ -2,15 +2,25 @@ const GetSupe = (id) => {
     fetch(`https://akabab.github.io/superhero-api/api/id/${id}.json`)
     .then(response => response.json())
     .then(json => {
-        document.getElementById('supeH').innerHTML = `<img style="width:300px; height:300px;" src="${json.images.sm}" >`
+        document.getElementById('supeH').innerHTML = ` <h2>${json.name}</h2>
+        <h4>${json.slug}</h4><img style="width:300px; height:300px;" src="${json.images.md}" >
+        ${getSckills(json)}`
     } )
 }
 
 GetSupe(1)
 const getNewSupe = () => {
     document.getElementById('getNewSupe').onclick = () => {
-        console.log('Get Supe')
-        GetSupe(Math.floor(Math.random() * 100))
+        GetSupe(Math.floor(Math.random() * 100)+1)
     }
 }
 getNewSupe()
+
+
+const getSckills = (char) => {
+    console.log('__________', Object.keys(char.powerstats))
+    let stat = Object.keys(char.powerstats).map(st => {
+        return `<p> ${st}: ${char.powerstats[st]} `
+    })
+    return stat.join('')
+}
