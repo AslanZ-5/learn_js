a78279417180f5dda739dc3e7683a655d241e115
мансур зурабов <asl.zurabov@gmail.com>
2022-09-11 12:49:02 +0300

    bank account with classes

--- classesJs/script.js
+++ classesJs/script.js
@@ -13,10 +13,53 @@ class Car {
     }
 }
 
-ferari = new Car('Ferari','black','360')
 
-console.log(ferari.name)
-console.log(ferari.color)
-console.log(ferari.speed)
-console.log(ferari.drive)
-console.log(ferari.stop)
+class BankAccount {
+    constructor (balance) {
+        this._balance = balance
+    }
+
+    getBalance () {
+        return this._balance
+    }
+
+    deposit (cash) {
+        this._balance += cash
+        console.log(`You'd successfully deposited $${cash} in cash. Your new balance is $${this.getBalance()}`)
+    }
+
+    withdraw (cash) {
+        if (cash > this._balance){
+            console.log("Your balance is not sufficient for this operation")
+            return
+        }
+        this._balance -= cash
+        console.log(`You'd successfully withdrawn $${cash} in cash. Your new balance is $${this.getBalance()}`)
+    }
+}
+
+acc = new BankAccount(500)
+// console.log(acc.getBalance())
+// acc.deposit(1000)
+// console.log(acc.getBalance())
+// console.log(acc._balance)
+// 
+
+const withdrawInp = document.getElementById('withdrawInp')
+const withdrawBtn = document.getElementById('withdraw')
+
+const depositInp = document.getElementById('depositInp')
+const depositBtn = document.getElementById('deposit')
+
+withdrawBtn.onclick = () => {
+    acc.withdraw(Number(withdrawInp.value))
+    document.getElementById('myBalance').innerText = acc.getBalance()
+}
+
+depositBtn.onclick = () => {
+    acc.deposit(Number(withdrawInp.value))
+    document.getElementById('myBalance').innerText = acc.getBalance()
+}
+
+
+
