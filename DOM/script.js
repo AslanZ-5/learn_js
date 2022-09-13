3d47f75dcfc093641b23f8234afa8701545a35dc
мансур зурабов <asl.zurabov@gmail.com>
2022-09-13 15:31:32 +0300

    parent and children elements

--- DOM/script.js
+++ DOM/script.js
@@ -36,4 +36,18 @@ const test3 = document.getElementById('img1')
 
 li.classList.add('myclasstest')
 // li.classList.remove('myclasstest')
-console.log(li.classList.contains('myclasstest'))
+// 
+const newul = ul.childNodes
+const newul1 = ul.firstChild
+const newul2 = ul.lastChild
+const newul3 = ul.children
+const newul4 = ul.firstElementChild
+const newul5 = ul.lastElementChild
+
+
+console.log(newul)
+console.log(newul1)
+console.log(newul2)
+console.log(newul3)
+console.log(newul4)
+console.log(newul5)
