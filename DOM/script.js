9ca8937ccfbc9b13aede7392f8a908ac312c5b57
мансур зурабов <asl.zurabov@gmail.com>
2022-09-13 14:36:47 +0300

    DOM manipulation "getelmentbyid by class

---
+++ DOM/script.js
@@ -0,0 +1,20 @@
+const test = document.getElementById('main-heading')
+test.onclick = () => {
+    test.style.color = 'blue'
+}
+
+
+const test2 = document.getElementsByClassName('list-items')
+const colors = ['blue','red','orange','green']
+c = 0
+for ( let i of test2){
+    i.style.color = colors[c]
+    c++
+    if (c >= colors.length){
+        c = 0
+    }
+}
+
+const ll = document.querySelector('li')
+ll.style.background = 'gray'
+console.log(ll)
