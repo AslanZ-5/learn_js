d4365777fe22284be0768d5818931c3830f0da44
мансур зурабов <asl.zurabov@gmail.com>
2022-09-13 15:12:17 +0300

    get set remove attribute

--- DOM/script.js
+++ DOM/script.js
@@ -1,3 +1,6 @@
+
+
+
 const test = document.getElementById('main-heading')
 test.onclick = () => {
     test.style.color = 'blue'
@@ -19,7 +22,14 @@ const ll = document.querySelector('li')
 ll.style.background = 'gray'
 
 
+
+
+
 const ul = document.querySelector('ul')
 const li = document.createElement('li')
 li.innerText = 'append'
 ul.append(li)
+li.setAttribute('id','main-heading')
+
+const test3 = document.getElementById('img1')
+console.log(test3.getAttribute('src'))
