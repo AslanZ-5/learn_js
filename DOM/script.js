e53fa36ead8c0fe0ce7d12ca0d1f460ad3a1a17e
мансур зурабов <asl.zurabov@gmail.com>
2022-09-13 15:36:34 +0300

    siblings of the element (previousElementSibling and nextElementSibling)

--- DOM/script.js
+++ DOM/script.js
@@ -37,17 +37,25 @@ const test3 = document.getElementById('img1')
 li.classList.add('myclasstest')
 // li.classList.remove('myclasstest')
 // 
-const newul = ul.childNodes
-const newul1 = ul.firstChild
-const newul2 = ul.lastChild
-const newul3 = ul.children
-const newul4 = ul.firstElementChild
-const newul5 = ul.lastElementChild
+// const newul = ul.childNodes
+// const newul1 = ul.firstChild
+// const newul2 = ul.lastChild
+// const newul3 = ul.children
+// const newul4 = ul.firstElementChild
+// const newul5 = ul.lastElementChild
 
 
-console.log(newul)
-console.log(newul1)
-console.log(newul2)
-console.log(newul3)
-console.log(newul4)
-console.log(newul5)
+// console.log(newul)
+// console.log(newul1)
+// console.log(newul2)
+// console.log(newul3)
+// console.log(newul4)
+// console.log(newul5)
+
+
+// SIBLINGS IS THE ELEMENT IN THE SAME LEVEL
+
+console.log(ul.previousSibling)
+console.log(ul.nextSibling)
+console.log(ul.previousElementSibling)
+console.log(ul.nextElementSibling)
